b8ada93a4a129658f035a15943b33ba6c3d0468a
Olena Melnykova <elena88melnykova@gmail.com>
2024-04-01 15:48:12 +0300

    UPDT2

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,17 @@
 import { useSelector } from 'react-redux';
-import { selectContacts } from '../../redux/contactsSlice';
+import { selectFilteredContacts } from '../../redux/contactsSlice';
 import Contact from '../Contact/Contact';
 import { StyledContactList } from './ContactList.styled';
-import { selectNameFilter } from '../../redux/filtersSlice';
 
 function ContactList() {
-  const selectedContacts = useSelector(selectContacts);
-  const filter = useSelector(selectNameFilter);
 
-  const contacts = selectedContacts.filter((contact) =>
-    typeof contact.name === 'string' && contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const contacts = useSelector(selectFilteredContacts);
 
   return (
     <StyledContactList>
-      {contacts.map(( { id, name, number }) => {
-        return <Contact key={id} id={id} name={name} number={number} />;
-        })}
+      {contacts.map(({ id, name, number }) => (
+        <Contact key={id} id={id} name={name} number={number} />
+      ))}
     </StyledContactList>
   );
 }
@@ -32,124 +27,6 @@ export default ContactList;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { useSelector } from 'react-redux';
-// import { selectContacts } from '../../redux/contactsSlice';
-// import Contact from '../Contact/Contact';
-// import { StyledContactList } from './ContactList.styled';
-// import { selectNameFilter } from '../../redux/filtersSlice';
-// import { ContactName } from '../Contact/Contact.styled';
-
-// function ContactList() {
-//   const selectedContacts = useSelector(selectContacts);
-//   const filter = useSelector(selectNameFilter);
-
-//   const contacts = selectedContacts.filter((contact ) => {
-//     const name = typeof contact.name === "string" ? contact.name.toLowerCase() : "";
-//     return name.includes(filter.toLowerCase());
-//   });
-
-//   return (
-//     <StyledContactList>
-//       {contacts.map(({contact}) => (
-//         <ContactName key={contact.id} id={contact} />
-//       ))}
-//     </StyledContactList>
-//   );
-// }
-
-// export default ContactList;
-
-
-
-// import { useSelector } from 'react-redux';
-// import { selectContacts } from '../../redux/contactsSlice';
-// import Contact from '../Contact/Contact'; // Исправлен импорт
-// import { StyledContactList } from './ContactList.styled';
-// import { selectNameFilter } from '../../redux/filtersSlice';
-
-// function ContactList() {
-//   const selectedContacts = useSelector(selectContacts);
-//   const filter = useSelector(selectNameFilter);
-
-//   const contacts = selectedContacts.filter((contact) => {
-//     const name = typeof contact.name === "string" ? contact.name.toLowerCase() : "";
-//     return name.includes(filter.toLowerCase());
-//   });
-
-//   return (
-//     <StyledContactList>
-//       {contacts.map(({ id, name, number }) => ( // Исправлена деструктуризация
-//         // Используем компонент Contact для рендеринга каждого контакта
-//         <Contact key={id} id={id} name={name} number={number} />
-//       ))}
-//     </StyledContactList>
-//   );
-// }
-
-// export default ContactList;
-
-
-
-
-
-
-
-
-
-
-
-
-// import { useSelector } from 'react-redux';
-// import { selectContacts } from '../../redux/contactsSlice';
-// import Contact from '../Contact/Contact';
-// import { StyledContactList } from './ContactList.styled';
-// import { selectNameFilter } from '../../redux/filtersSlice';
-
-// function ContactList() {
-//   const selectedContacts = useSelector(selectContacts);
-//   const filter = useSelector(selectNameFilter);
-
-//   const contacts = selectedContacts.filter((contact) => {
-//     const name = typeof contact.name === "string" ? contact.name.toLowerCase() : "";
-//     return name.includes(filter.toLowerCase());
-//   });
-
-//   return (
-//     <StyledContactList>
-//       {contacts.map(({ id, name, number }) => (
-//         <Contact key={id} id={id} name={name} number={number} />
-//       ))}
-//     </StyledContactList>
-//   );
-// }
-
-// export default ContactList;
 
 
 
